17ce70e1ea3cb7edc2a066ac071bc030c8a1ffb5
TJ Koblentz <tj.koblentz@gmail.com>
2011-06-26 15:24:23 -0700

    Moving to full on framework.

--- config/database.js
+++
@@ -1,3 +0,0 @@
-var mongodb     = require("../lib/mongodb"),
-    mongoserver = new mongodb.Server('localhost', mongodb.Connection.DEFAULT_PORT, {}),
-    mongoconn   = new mongodb.Db('blog', mongoserver, {});
